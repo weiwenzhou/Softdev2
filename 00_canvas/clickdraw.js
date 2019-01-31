0733f2e3f568613252c7787f4667eb87c0c1422d
wzhou2 <wzhou2@stuy.edu>
2019-01-30 22:10:43 -0500

    paint done

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -1,7 +1,36 @@
 var canvas = document.getElementById("slate");
 var ctx = canvas.getContext("2d");
 
+// Canvas Attributes
+var width = canvas.width; // width
+var height = canvas.height; // height
+
+// State var
+var state = true; // true = rectangle, false = dot
+
+var clear_canvas = function() {
+    // Clears the canvas
+    ctx.clearRect(0,0, width, height);
+}
+
+var toggle = function() {
+    // Change the shape between rectangle and dot
+    state = !state;
+    if (state) {
+        document.getElementById("state").innerHTML = "Rectangle";
+    } else {
+        document.getElementById("state").innerHTML = "Dot";
+    }
+}
+
+// when click in canvas draw either a rectangle or ellipse
 canvas.addEventListener("click", function(e){
-    console.log(e);
-    ctx.fillRect(e.clientX, e.clientY, 100,100);
+    //console.log(e);
+    if (state) {
+        ctx.fillRect(e.offsetX-50, e.offsetY-50, 100, 100);
+    } else {
+        ctx.beginPath();
+        ctx.ellipse(e.offsetX, e.offsetY, 5, 5, 0, 0, 360);
+        ctx.fill();
+    }
 })
