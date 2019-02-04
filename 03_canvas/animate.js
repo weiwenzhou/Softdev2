44c4aa4ef22ccd11f8b746a5dc4995fde2130f16
wzhou2 <wzhou2@stuy.edu>
2019-02-04 14:06:24 -0500

    classwork

---
+++ 03_canvas/animate.js
@@ -0,0 +1,51 @@
+// Datapen - Wei Wen Zhou, Rubin Peci
+// SoftDev2 pd8
+// K03 --
+// 2019-02-06
+
+// Canvas Attributes
+var canvas = document.getElementById("playground");
+var ctx = canvas.getContext("2d");
+var width = canvas.width; // width
+var height = canvas.height; // height
+ctx.fillStyle = "#ff0000"; // set the fillstyle to red
+
+// State var
+var state; // animation id
+
+// Circle var
+var radius = 2;
+var speed = 2;
+
+// Animate
+document.getElementById("circle").addEventListener("click", function(e){
+    state = window.requestAnimationFrame(animate);
+})
+
+var animate = function() {
+    if (radius >= width/2) {
+        speed = speed*-1; // Contract
+    }
+    if (radius <= 0) {
+        speed = speed*-1; // Expand
+    }
+    radius = radius + speed;
+    clear_canvas();
+    drawEllipse(250, 250, radius);
+
+    state = window.requestAnimationFrame(animate);
+}
+
+// Stop
+
+var drawEllipse = function(mouseX, mouseY, radius){
+    // Draws the ellipse
+    ctx.ellipse(mouseX, mouseY, radius, radius, 0, 0, 360);
+    ctx.stroke();
+    ctx.fill();
+}
+
+var clear_canvas = function() {
+    // Clears the canvas
+    ctx.clearRect(0,0, width, height);
+}
