ecfe6b516a87d67f8f67551276d31271956c0cc7
wzhou2 <wzhou2@stuy.edu>
2019-02-06 03:36:33 -0500

    flesh out the animation

--- 03_canvas-anim/animate.js
+++ 03_canvas-anim/animate.js
@@ -12,36 +12,66 @@ ctx.fillStyle = "#ff0000"; // set the fillstyle to red
 
 // State var
 var state; // animation id
+var animated; // whether the circle is being animate or not
 
 // Circle var
-var radius = 2;
-var speed = 2;
+var radius = 2; // Initial radius
+var speed = 2; // Speed at which the radius is expanding/contracting
 
 // Animate
 document.getElementById("circle").addEventListener("click", function(e){
-    state = window.requestAnimationFrame(animate);
+    // Check whether the circle is being animated yet. If not animate it
+    if (animated) {
+        e.preventDefault();
+    } else {
+        state = window.requestAnimationFrame(animate);
+    }
 })
 
 var animate = function() {
+    // Draw a circle expanding from the center of the canvas and upon
+    // reaching the edge of the canvas contracts
+
     // Increase/Decrease Radius
     radius = radius + speed;
-    console.log(radius);
+
     if (radius >= width/2) {
         speed = speed*-1; // Contract
     }
     if (radius <= 0) {
         speed = speed*-1; // Expand
     }
+
     clear_canvas();
     drawEllipse(250, 250, radius);
+
+    // Set animated to true
+    animated = true;
 
     state = window.requestAnimationFrame(animate);
 }
 
 // Stop
+document.getElementById("stop").addEventListener("click", function(e){
+    // Check whether the circle is being animated or not
+    // If animated stop the animation else do nothing
+    if (animate) {
+        stopAnimate();
+    } else {
+        e.preventDefault();
+    }
+})
+
+var stopAnimate = function() {
+    // Set animated to false;
+    animated = false;
+    
+    window.cancelAnimationFrame(state);
+}
 
 var drawEllipse = function(mouseX, mouseY, radius){
     // Draws the ellipse
+    ctx.beginPath();
     ctx.ellipse(mouseX, mouseY, radius, radius, 0, 0, 360);
     ctx.stroke();
     ctx.fill();
