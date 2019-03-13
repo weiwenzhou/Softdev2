c7b53f23aabe207221d1e9bf86f77b05528eeffb
wzhou2 <wzhou2@stuy.edu>
2019-03-13 03:28:28 -0400

    finish hw

--- 09_svg/svg01.js
+++ 09_svg/svg01.js
@@ -1,13 +1,51 @@
+// DOM Variables
 var pic = document.getElementById("vimage");
-console.log(pic)
-var c = document.createElementNS("http://www.w3.org/2000/svg", "circle")
-c.setAttribute( "cx", 0)
-c.setAttribute( "cy", 0)
-c.setAttribute( "r", "100")
-c.setAttribute( "fill", "red")
-c.setAttribute( "stroke", "black")
+var button = document.getElementById("but_clear");
 
-pic.appendChild(c)
+// Coordinates
+var x = null;
+var y = null;
+
+// Event Listeners
+pic.addEventListener('click', function(e) {
+    // console.log(e)
+    if (x != null) {
+        line(pic, x, y, e.offsetX, e.offsetY, "black");
+    }
+    x = e.offsetX;
+    y = e.offsetY;
+    circle(pic, x, y, "10", "red", "black");
+});
+
+button.addEventListener('click', function(e){
+    clear()
+});
+
+// Functions
+var circle = function(canvas, x, y, r, fill, stroke) {
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c.setAttribute( "cx", x);
+    c.setAttribute( "cy", y);
+    c.setAttribute( "r", r);
+    c.setAttribute( "fill", fill);
+    c.setAttribute( "stroke", stroke);
+
+    canvas.appendChild(c);
+}
+
+var line = function(canvas, x1, y1, x2, y2, stroke) {
+    var l = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    l.setAttribute( "x1", x1);
+    l.setAttribute( "y1", y1);
+    l.setAttribute( "x2", x2);
+    l.setAttribute( "y2", y2);
+    l.setAttribute( "stroke", stroke);
+
+    canvas.appendChild(l);
+}
 
 var clear = function() {
+    pic.innerHTML = '';
+    x = null;
+    y = null;
 }
