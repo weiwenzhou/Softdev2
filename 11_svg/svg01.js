ecb07f0278648593aefdef08d2bba71b426a4d49
wzhou2 <wzhou2@stuy.edu>
2019-03-15 15:09:31 -0400

    begin updation

--- 11_svg/svg01.js
+++ 11_svg/svg01.js
@@ -1,10 +1,16 @@
 // DOM Variables
 var pic = document.getElementById("vimage");
 var button = document.getElementById("but_clear");
+var move_button = document.getElementById("move");
 
 // Coordinates
 var x = null;
 var y = null;
+
+// Animation Variables
+xspeed = 1;
+yspeed = 1;
+state;
 
 // Event Listeners
 pic.addEventListener('click', function(e) {
@@ -67,4 +73,27 @@ var clear = function() {
     pic.innerHTML = '';
     x = null;
     y = null;
+}
+
+// Animate
+var animate = function() {
+    // Cancel existing animation
+    window.cancelAnimationFrame(state);
+
+    // Touches the horizontal edge
+    if (dvdX <= 0 || dvdX + dvd_width >= width) {
+        xspeed = -1*xspeed;
+    }
+
+    // Touches the vertical edge
+    if (dvdY <= 0 || dvdY + dvd_height >= height) {
+        yspeed = -1*yspeed;
+    }
+
+    // Apply translations
+    dvdX = dvdX + xspeed;
+    dvdY = dvdY + yspeed;
+    drawEllipse(width/2, height/2, radius);
+
+    state = window.requestAnimationFrame(animateCircle);
 }
