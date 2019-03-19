41b4483dc1b228e25d4391c8fc75df33af949126
wzhou2 <wzhou2@stuy.edu>
2019-03-19 14:06:42 -0400

    classwork

---
+++ 13_svg/svg.js
@@ -0,0 +1,13 @@
+// Wei Wen Zhou
+// SoftDev2 pd8
+// K<13> -- Driving Deeper into D3
+// 2019-03-20
+
+// Dataset
+var data = [4,8,15,16,23,42];
+
+// Scaling function
+var scale = d3.scaleLinear().domain([0, data.max(data)]).range([0, 420]);
+
+d3.select(".chart").selectAll("div").data(data).enter().append("div")
+.style("width", function(d) {return scale(d) + "px";}).text(function(d) {return d;});
