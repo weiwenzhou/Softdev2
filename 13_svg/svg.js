d6946c0e8d50cc6a5d6bd566bf6dcaaf8252a769
wzhou2 <wzhou2@stuy.edu>
2019-03-20 07:58:01 -0400

    fix it

--- 13_svg/svg.js
+++ 13_svg/svg.js
@@ -7,7 +7,13 @@
 var data = [4,8,15,16,23,42];
 
 // Scaling function
-var scale = d3.scaleLinear().domain([0, data.max(data)]).range([0, 420]);
+var x = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, 420]);
 
-d3.select(".chart").selectAll("div").data(data).enter().append("div")
-.style("width", function(d) {return scale(d) + "px";}).text(function(d) {return d;});
+d3.select(".chart")
+    .selectAll("div")
+    .data(data)
+    .enter().append("div")
+    .style("width", function(d) { return x(d) + "px"; })
+    .text(function(d) { return d; });
