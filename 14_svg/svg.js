86184f2b4fdb84ba6c87a2ca4d33f2df1620d090
wzhou2 <wzhou2@stuy.edu>
2019-03-26 09:00:50 -0400

    catching up

---
+++ 14_svg/svg.js
@@ -0,0 +1,10 @@
+// Get the body
+var body = d3.select("body");
+
+// Get the chart div
+var chart = d3.selectAll("div");
+
+// Get the bar divs + chart div 
+var bars = d3.selectAll("div");
+
+bars.append("span").text("A PPENDING....")
