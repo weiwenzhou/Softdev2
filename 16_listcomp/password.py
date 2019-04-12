38ce16a09a0912e80568f26a36dd97e43f00fccd
wzhou2 <wzhou2@stuy.edu>
2019-04-12 03:07:15 -0400

    finish hw

--- 16_listcomp/password.py
+++ 16_listcomp/password.py
@@ -1,12 +1,12 @@
 # Wei Wen Zhou
 # SoftDev2 pd8
-# K< > --
+# K<16> -- Do You Even List?
 # 2019-04-11
 
 UC_LETTERS = 'QWERTYUIOPASDFGHJKLZXCVBNM'
 LC_LETTERS = 'qwertyuiopasdfghjklzxcvbnm'
 NUMBERS = '1234567890'
-SPECIALS = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+SPECIALS = ".?!&#,;:-_*"
 
 # Function checker
 def check(func, val):
@@ -24,21 +24,38 @@ def sum(list):
 
 # Return true if password contains UC/LC letters, numbers, and symbols else false
 def checkRequirements(password):
-    if len(password) < 5:
-        return False
-    else:
-        return sum([1 if x in UC_LETTERS else 0 for x in password])
+    return len([x for x in password if x in UC_LETTERS]) > 0  and len([x for x in password if x in LC_LETTERS]) > 0 and len([x for x in password if x in NUMBERS]) > 0
+
 # Return a number between 0 and scale
 def strength(password, scale=1):
-    pass
+    if len(password) == 0:
+        return 0
 
-if True:
+    E = len(password)/4
+    U = len([x for x in password if x in UC_LETTERS])
+    L = len([x for x in password if x in LC_LETTERS])
+    N = len([x for x in password if x in NUMBERS])
+    S = len([x for x in password if x in SPECIALS])
+
+    val = [ (x - E)**2/E for x in [U, L, N, S] ]
+    total = sum(val)
+
+    if total:
+        return scale/total
+    return scale
+
+if 0:
     check( sum ([1,1,1,1]), 4)
     check( sum ([]), 0)
     check( sum ([x for x in range(10)]), 45)
 
-if True:
+if 0:
     check( checkRequirements(''), False)
     check( checkRequirements('a'), False)
     check( checkRequirements('a124'), False)
+    check( checkRequirements('aA124'), True)
+    check( checkRequirements('aA!'), False)
     check( checkRequirements('aA1!'), True)
+
+p = "password1"
+print(strength(p, 10))
